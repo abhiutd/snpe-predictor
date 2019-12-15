c9a07dcc94064c1aa6b725a0ffaff4c5ef7e177d
abhiutd <abhibits2012@gmail.com>
2019-12-15 16:23:03 -0600

    update bindings

--- lib.go
+++ lib.go
@@ -1,5 +1,5 @@
 package snpe
 
 // #cgo CXXFLAGS: -std=c++11 -I${SRCDIR}/cbits -O3 -Wall -g -Wno-sign-compare -Wno-unused-function  -I/home/as29/my_snpe/snpe-1.32.0.555/include/zdl -I/home/as29/my_gles
-// #cgo LDFLAGS: -lstdc++ -L/home/as29/my_android_ndk/android-ndk-r19c -llog -L/home/as29/my_android_ndk/android-ndk-r19c/platforms/android-28/arch-arm64/usr/lib -lEGL -lGLESv3 -L/home/as29/my_snpe/snpe-1.32.0.555/lib/aarch64-android-clang6.0 -lc++_shared -lhta -lPlatformValidatorShared -lPSNPE -lsnpe_adsp -lsnpe_dsp_domains -lsnpe_dsp_domains_system -lsnpe_dsp_domains_v2 -lsnpe_dsp_domains_v2_system -lSNPE_G -lSNPE -lsymphony-cpu
+// #cgo LDFLAGS: -lstdc++ -L/home/as29/my_android_ndk/android-ndk-r19c -L/home/as29/my_android_ndk/android-ndk-r19c/platforms/android-28/arch-arm64/usr/lib -llog -lEGL -lGLESv3 -L/home/as29/my_snpe/snpe-1.32.0.555/lib/aarch64-android-clang6.0 -lc++_shared -lPlatformValidatorShared -lPSNPE -lSNPE_G -lSNPE -lsymphony-cpu
 import "C"
